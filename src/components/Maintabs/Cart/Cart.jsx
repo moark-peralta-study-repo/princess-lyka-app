6ff8904cecf2e5b7fd31e30205fdd5eccb07237e
moark-peralta-study-repo <markperaltaasd@gmail.com>
2026-02-17 18:46:01 +0800

    ui tweaks

--- src/components/Maintabs/Cart/Cart.jsx
+++ src/components/Maintabs/Cart/Cart.jsx
@@ -6,11 +6,15 @@ import CartItemCard from "../../ui/CartItem/CartItem";
 import { DUMMY_ITEMS } from "../../../data/dummyProducts";
 import ScreenHeader from "../../ui/ScreenHeader/ScreenHeader";
 import PrimaryBtn from "../../ui/PrimaryBtn";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Cart() {
 	const [items, setItems] = useState(DUMMY_ITEMS);
 
 	const navigation = useNavigation();
+	const tabBarH = useBottomTabBarHeight();
+	const inset = useSafeAreaInsets();
 
 	function emulateCheckOut() {
 		navigation.navigate("Checkout");
@@ -37,31 +41,36 @@ export default function Cart() {
 		<SafeAreaView style={styles.safe}>
 			<View style={styles.container}>
 				<ScreenHeader title="Cart" />
-			</View>
 
-			<ScrollView contentContainerStyle={[styles.list, { paddingBottom: 120 }]}>
-				{items.map((item) => (
-					<CartItemCard
-						title={item.title}
-						meta={item.meta}
-						qty={item.qty}
-						onDec={() => dec(item.id)}
-						onInc={() => inc(item.id)}
-						onRemove={() => remove(item.id)}
-						key={item.id}
-					/>
-				))}
-
-				<View style={{ height: 90 }} />
-
-				<PrimaryBtn
-					style={{ position: "absolute", left: 16, right: 16, bottom: 18 }}
-					onPress={emulateCheckOut}
-					variant={"primary"}
+				<ScrollView
+					contentContainerStyle={[
+						styles.list,
+						{ paddingBottom: tabBarH + inset.bottom },
+					]}
 				>
-					<Text style={styles.checkoutText}>Check Out</Text>
-				</PrimaryBtn>
-			</ScrollView>
+					{items.map((item) => (
+						<CartItemCard
+							title={item.title}
+							meta={item.meta}
+							qty={item.qty}
+							onDec={() => dec(item.id)}
+							onInc={() => inc(item.id)}
+							onRemove={() => remove(item.id)}
+							key={item.id}
+						/>
+					))}
+
+					<View style={{ height: 90 }} />
+
+					<PrimaryBtn
+						style={{ position: "absolute", left: 16, right: 16, bottom: 150 }}
+						onPress={emulateCheckOut}
+						variant={"primary"}
+					>
+						<Text style={styles.checkoutText}>Check Out</Text>
+					</PrimaryBtn>
+				</ScrollView>
+			</View>
 		</SafeAreaView>
 	);
 }
--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -22,7 +22,7 @@ const Stack = createNativeStackNavigator();
 export default function AppNavigator() {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator>
+			<Stack.Navigator screenOptions={{ headerShown: false }}>
 				<Stack.Screen name="HomeScreen" component={HomeScreen} />
 				<Stack.Screen name="Maintabs" component={Maintabs} />
 				<Stack.Screen name="Auth" component={AuthNavigator} />
