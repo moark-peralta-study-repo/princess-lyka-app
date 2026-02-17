0195496fa957780a635a7188a03a89dbb1c38657
moark-peralta-study-repo <markperaltaasd@gmail.com>
2026-02-17 20:01:46 +0800

    move chatbot screen to maintab navighator

--- src/navigation/Maintabs.jsx
+++ src/navigation/Maintabs.jsx
@@ -4,11 +4,8 @@ import Cart from "../components/Maintabs/Cart/Cart";
 import UserProfile from "../components/Maintabs/UserProfile/UserProfile";
 import Wishlist from "../components/Maintabs/Wishlist/Wishlist";
 
-import HomeIcon from "../../assets/home.svg";
-import CartIcon from "../../assets/cart.svg";
-import ProfileIcon from "../../assets/avatar.svg";
-import WishlistIcon from "../../assets/wishlist.svg";
 import CustomBottomBar from "../components/ui/CustomeBottomBar/CustomBottomBar";
+import ChatbotScreen from "../screens/ChatBot/ChatbotScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -22,6 +19,7 @@ export default function Maintabs() {
 			<Tab.Screen name="Cart" component={Cart} />
 			<Tab.Screen name="Wishlist" component={Wishlist} />
 			<Tab.Screen name="Profile" component={UserProfile} />
+			<Tab.Screen name="ChatBot" component={ChatbotScreen} />
 		</Tab.Navigator>
 	);
 }
---
+++ src/screens/ChatBot/ChatbotScreen.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ChatbotScreen() {
+	return (
+		<View>
+			<Text>Chat Bot</Text>
+		</View>
+	);
+}
